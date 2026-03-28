5f4e48c01ad31e5c9ef2f8d8101b6c0b2a132518
vtpatrickchu <vtpatrickchu@gmail.com>
2026-03-27 21:00:38 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
-
 import "./globals.css";
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Trading Calendar Dashboard",
-  description: "Upload a CSV to generate trading calendar charts.",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
